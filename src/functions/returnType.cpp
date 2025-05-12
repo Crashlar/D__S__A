c8ee6777e966ba69a9319d6d0e084e785ee6704f
Mukesh Kumar <mukeshkumar.in25@gmail.com>
2025-05-13 02:01:20 +0530

    continues

--- src/functions/returnType.cpp
+++ src/functions/returnType.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<cmath>
 using namespace std;
 int sum(int x , int y ){
     return x + y ; 
@@ -8,7 +9,8 @@ int main(){
     // cout <<  sum( 40 , 43 )<< endl ;
     cout << min( 40 , 34)<< endl ;
     cout << max( 40 , 34)<< endl ;
-    
+    cout << sqrt( 40)<< endl ;
+
 
     return 0;
 }
