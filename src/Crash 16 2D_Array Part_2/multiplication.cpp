16bb50c5c37156d4c17ff3910bd5b64dd958cbbd
Mukesh Kumar <mukeshkumar.in25@gmail.com>
2025-06-01 00:15:29 +0530

    completed Crash 16

--- src/Crash 16 2D_Array Part_2/multiplication.cpp
+++ src/Crash 16 2D_Array Part_2/multiplication.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
-
-int main() {
+/*
+int main()
+{
     int r1, c1, r2, c2;
 
     // Input dimensions
@@ -11,8 +12,9 @@ int main() {
     cout << "Enter rows and columns of second matrix: ";
     cin >> r2 >> c2;
 
-     // Check if multiplication is possible
-    if (c1 != r2) {
+    // Check if multiplication is possible
+    if (c1 != r2)
+    {
         cout << "Matrix multiplication not possible! (columns of A must equal rows of B)" << endl;
         return 0;
     }
@@ -22,31 +24,40 @@ int main() {
 
     // Input first matrix
     cout << "Enter elements of first matrix:" << endl;
-    for (int i = 0; i < r1; ++i) {
-        for (int j = 0; j < c1; ++j) {
+    for (int i = 0; i < r1; ++i)
+    {
+        for (int j = 0; j < c1; ++j)
+        {
             cin >> A[i][j];
         }
     }
 
     // Input second matrix
     cout << "Enter elements of second matrix:" << endl;
-    for (int i = 0; i < r2; ++i) {
-        for (int j = 0; j < c2; ++j) {
+    for (int i = 0; i < r2; ++i)
+    {
+        for (int j = 0; j < c2; ++j)
+        {
             cin >> B[i][j];
         }
     }
 
     // Initialize result matrix with 0
-    for (int i = 0; i < r1; ++i) {
-        for (int j = 0; j < c2; ++j) {
+    for (int i = 0; i < r1; ++i)
+    {
+        for (int j = 0; j < c2; ++j)
+        {
             result[i][j] = 0;
         }
     }
 
     // Matrix multiplication logic
-    for (int i = 0; i < r1; ++i) {
-        for (int j = 0; j < c2; ++j) {
-            for (int k = 0; k < c1; ++k) {
+    for (int i = 0; i < r1; ++i)
+    {
+        for (int j = 0; j < c2; ++j)
+        {
+            for (int k = 0; k < c1; ++k)
+            {
                 result[i][j] += A[i][k] * B[k][j];
             }
         }
@@ -54,12 +65,87 @@ int main() {
 
     // Output result
     cout << "Resultant matrix after multiplication:" << endl;
-    for (int i = 0; i < r1; ++i) {
-        for (int j = 0; j < c2; ++j) {
+    for (int i = 0; i < r1; ++i)
+    {
+        for (int j = 0; j < c2; ++j)
+        {
             cout << result[i][j] << " ";
         }
         cout << endl;
     }
 
     return 0;
+}
+
+*/
+
+int main()
+{
+    int m;
+    cout << "Enter rows of 1st matrix : ";
+    cin >> m;
+
+    int n;
+    cout << "Enter cols of 1st matrix : ";
+    cin >> n;
+
+    int p;
+    cout << "Enter rows of 2nd Matrix : ";
+    cin >> p;
+    int q;
+    cout << "Enter cols of 2nd Matrix : ";
+    cin >> q;
+
+    if (n == p)
+    {
+        int a[m][n];
+        cout << "Enter Elements of 1st matrix : "<< endl;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cin >> a[i][j];
+            }
+        }
+        int b[p][q];
+        cout << "Enter elements of 2nd Matrix  :"<< endl;;
+        for (int i = 0; i < p; i++)
+        {
+            for (int j = 0; j < q; j++)
+            {
+                cin >> b[i][j];
+            }
+        }
+        // resultant matrix
+        int res[m][q];
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < q; j++)
+            {
+                // multiply
+                res[i][j] = 0; // set initial value
+                for (int k = 0; k < p; k++)
+                {
+                    res[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        // printing the matrix
+        cout << "After matrx Multiplication : ";
+        cout << endl;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < q; j++)
+            {
+                cout << res[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    else
+        // n != p
+        cout << "The matrix multiplication is not possible.";
+
+        return 0 ; 
 }
--- src/Crash 16 2D_Array Part_2/waveform.cpp
+++ src/Crash 16 2D_Array Part_2/waveform.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+/*
 int main()
 {
     int m;
@@ -23,6 +23,7 @@ int main()
     // wave print
     for (int i = 0; i < m; i++)
     {
+        // even row
         if (i % 2 == 0)
         {
             for (int j = 0; j < n; j++)
@@ -32,14 +33,77 @@ int main()
         }
         else
         {
-            for (int j = n-1; j >= 0 ; j--)
+            for (int j = n - 1; j >= 0; j--)
             {
                 cout << arr[i][j] << " ";
             }
-            
         }
-        
-       
+    }
+
+    return 0;
+}
+*/
+
+/*
+int main()
+{
+    int m = 3, n = 3;
+
+    int arr[m][n] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
+    // wave print
+    for (int i = n - 1; i >= 0; i--)
+    {
+        // even row
+        if (i % 2 == 0)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+        else
+        {
+            for (int j = n - 1; j >= 0; j--)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+    }
+
+    return 0;
+}
+*/
+
+// colm wise printing
+
+int main()
+{
+    int m = 3, n = 3;
+    int arr[m][n] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        // even row
+        if (j % 2 == 0)
+        {
+            for (int i = 0; i < m; i++)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+        else
+        {
+            for (int i = m - 1; i >= 0; i--)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
     }
 
     return 0;
